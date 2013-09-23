07a9b1c365ff2256f625d7369444b447c7626dde
root <rui.wax@gmail.com>
2013-09-22 21:59:15 -0700

    Until Exercise 11 Complete

--- tree/btree.c
+++ tree/btree.c
@@ -73,13 +73,37 @@ root->right= newNode(3);
 return root;	
 }
 
-node_t* build1233(){
+node_t* build1234(){
 node_t *root=NULL;
 root=insertNode(root,4);
 root=insertNode(root,2);
 root=insertNode(root,1);
 root=insertNode(root,3);
 root=insertNode(root,6);
+//root=insertNode(root,16);
+////root=insertNode(root,20);
+////root=insertNode(root,21);
+////root=insertNode(root,17);
+////root=insertNode(root,18);
+////root=insertNode(root,19);
+////root=insertNode(root,4);
+////root=insertNode(root,3);
+////root=insertNode(root,2);
+////root=insertNode(root,1);
+//
+//
+//
+return root;
+//}
+//
+}
+node_t* build1233(){
+node_t *root=NULL;
+root=insertNode(root,4);
+root=insertNode(root,2);
+root=insertNode(root,1);
+root=insertNode(root,3);
+root=insertNode(root,5);
 //root=insertNode(root,16);
 //root=insertNode(root,20);
 //root=insertNode(root,21);
@@ -209,8 +233,10 @@ int hasPathSum2(node_t *root, int sum) {
 } 
 
 void printPathsRecur(node_t *node, int path[], int pathLen){
-        if(node==NULL){
+        if(node->left==NULL && node->right==NULL){
                int i;
+	       path[pathLen]=node->value;
+	       pathLen+=1;	
                printf("Path: ");
                for(i=0;i<pathLen;i++){
                        printf("%d ",path[i]);
@@ -227,7 +253,7 @@ void printPathsRecur(node_t *node, int path[], int pathLen){
 
 void printPaths(node_t *root) {
 	int pathLen=0;
-	int path[50];
+	int path[500];
 
 	if(root==NULL)
 		return;
@@ -235,11 +261,89 @@ void printPaths(node_t *root) {
 		printPathsRecur(root, path, pathLen);
 }
 
-                                    
+                                   
+void mirror(node_t *root){
+	node_t *helper=NULL;
+	if(root->left==NULL && root->right==NULL)
+		return;
+	
+	helper=root->right;
+	root->right=root->left;
+	root->left=helper;
+	mirror(root->right);
+	mirror(root->left);
+	
+}
+
+
+void printArray(int ints[], int len) {
+  int i;
+  for (i=0; i<len; i++) {
+    printf("%d ", ints[i]);
+  }
+  printf("\n");
+}
+
+void printPathsRecur1(node_t* node, int path[], int pathLen) { 
+  if (node==NULL) return;
+
+  path[pathLen] = node->value; 
+  pathLen++; 
+  if (node->left==NULL && node->right==NULL) { 
+    printArray(path, pathLen); 
+  } 
+  else { 
+    printPathsRecur1(node->left, path, pathLen); 
+    printPathsRecur1(node->right, path, pathLen); 
+  } 
+} 
+
+void printPaths1(node_t* node) {
+  int path[1000];
+  printPathsRecur1(node, path, 0);
+}
+
+
+
+
+
+void doubleTree(node_t *root)
+{
+	if(root==NULL)
+		return;
+	
+	doubleTree(root->left);
+	doubleTree(root->right);
+	//printf("A: %d \n", root->value );
+	node_t *helper = NULL;
+	helper = root->left;
+		//printf("Left Value: %d\n", helper->value);
+	root->left = newNode(root->value);
+		//printf("New Left Value: %d\n", root->left->value);
+        root->left->left = helper;
+		//printf("New Left Left Value: %d\n", root->left->left->value);
+}
+
+int sameTree(node_t *root, node_t *second ){
+	if(root==NULL && second==NULL)
+		return(1);
+
+        else if(root!=NULL && second!=NULL){
+		printf("Second Value %d\n", second->value);
+		printf("Root value %d\n", root->value);
+		return(root->value==second->value &&
+		       sameTree(root->left, second->left) &&
+		       sameTree(root->right, second->right));
+	}
+	else 
+		return(0);
+}
 
 int main(){
 	node_t *root=NULL;
+	node_t *second=NULL;
 	root=build1233();
+	second=build1234();
 	printf("Valueof Root: %d \n",root->value);
 	printf("Size of Tree: %d \n", size(root));
 	printf("Max Depth of Tree: %d \n", maxDepth(root));
@@ -251,5 +355,12 @@ int main(){
         printTree3(root);
 	printf("\n%d\n", hasPathSum2(root,9));
 	printPaths(root);
+	printf("\n");
+	//doubleTree(root);
+	printf("\n");
+        printPaths1(root);
+	printf("Same Tree: %d\n",sameTree(root, second));
+	free(root);
+
 return 0;
 }
